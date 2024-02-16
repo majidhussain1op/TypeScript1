2e801522160154e067491a50f8a62562412e2acb
majidhussain1op <142897507+majidhussain1op@users.noreply.github.com>
2024-02-16 19:15:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+console.log("Hello Majid Hussain Meerani");
+let PersonName = ("Hello Eric, would you like to learn some python today?!");
+console.log(PersonName);
---
+++ main.ts
@@ -0,0 +1,5 @@
+console.log ("hello world");
+console.log("Hello Majid Hussain Meerani")
+
+let PersonName : string = ("Hello Eric, would you like to learn some python today?!")
+console.log(PersonName)
